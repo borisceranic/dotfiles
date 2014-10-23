057a55971062f60fb0036d0d7550988ff3fe09c5
Boris Ceranic <me@sosko.in.rs>
2014-10-23 16:54:55 +0200

    Add tmux and zsh default configuration

--- install.sh
+++ install.sh
@@ -11,3 +11,11 @@ mv -f $HOME/.gvimrc $HOME/$BKUP.gvimrc
 ln -s $DOTFILES/vim/vim $HOME/.vim
 ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
 ln -s $DOTFILES/vim/gvimrc $HOME/.gvimrc
+
+echo Installing zsh files
+mv -f $HOME/.zshrc $HOME/$BKUP.zshrc
+ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
+
+echo Installing tmux files
+mv -f $HOME/.tmux.conf $HOME/$BKUP.tmux.conf
+ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
