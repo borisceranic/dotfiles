1faf8654efc88c41ba78ee76f67d6589788d8ae5
Boris Ceranic <me@sosko.in.rs>
2015-05-05 00:45:27 +0200

    Add auto-loading support for jenv (java environments)

--- zsh/zshrc.osx.zsh
+++ zsh/zshrc.osx.zsh
@@ -15,3 +15,6 @@ export LSCOLORS="ExGxFxDxCxDxDxhbhdacEc"
 # nvm
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
+
+# invoke jenv to configure java development environment
+eval "$(jenv init -)"
