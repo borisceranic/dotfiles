77b2a5707e7a9163df43866ecc659d22aef551d9
Boris Ceranic <me@sosko.in.rs>
2015-06-09 14:47:06 +0200

    Add zgen (zsh plugin manager)

--- zsh/zshrc.osx.zsh
+++ zsh/zshrc.osx.zsh
@@ -2,12 +2,6 @@
 path=("$HOME/sbin" "/usr/local/mysql/bin" $path)
 export PATH
 
-## brew zsh-completions
-fpath=(/usr/local/share/zsh-completions $fpath)
-
-## brew zsh-history-substring-search
-source /usr/local/opt/zsh-history-substring-search/zsh-history-substring-search.zsh
-
 # ls colors
 export CLICOLOR=1
 export LSCOLORS="ExGxFxDxCxDxDxhbhdacEc"
