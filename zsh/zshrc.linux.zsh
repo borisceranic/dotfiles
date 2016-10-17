17967a7cf684e32277907f8531c4df01fcccc723
Boris Ceranic <me@sosko.in.rs>
2016-10-17 12:52:58 +0200

    Various zsh tweaks, including alises

--- zsh/zshrc.linux.zsh
+++ zsh/zshrc.linux.zsh
@@ -1,3 +1,5 @@
 # ls config
 alias ls='ls -G'
 export LSCOLORS='di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35:*.rpm=90'
+
+alias ports="netstat -nlpt | grep LISTEN"
--- zsh/zshrc.osx.zsh
+++ zsh/zshrc.osx.zsh
@@ -3,11 +3,20 @@ path=("$HOME/sbin" $path)
 export PATH
 
 # homebrew cask default options
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export HOMEBREW_GITHUB_API_TOKEN=""
+export HOMEBREW_NO_ANALYTICS=1
+
+nvm_prefix=$(brew --prefix nvm)
+if [ ! -z $nvm_prefix ]; then
+	export NVM_DIR="$HOME/.nvm"
+	source "$(brew --prefix nvm)/nvm.sh"
+fi
 
 # ls colors
 export CLICOLOR=1
 export LSCOLORS="ExGxFxDxCxDxDxhbhdacEc"
 
-# invoke jenv to configure java development environment
-eval "$(jenv init -)"
+alias brew-upgrade="brew upgrade --all; brew cleanup; brew cask cleanup; brew doctor; brew missing"
+alias ports="netstat -p tcp -a -n | grep LISTEN "
+alias routes="netstat -r -n"
+alias flush-dns="sudo killall -HUP mDNSResponder"
