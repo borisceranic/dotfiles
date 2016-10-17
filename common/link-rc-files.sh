158d307cda208ae35e3132a24fd2ec2661ec7326
Boris Ceranic <me@sosko.in.rs>
2016-10-17 12:49:34 +0200

    Remove tmux and Sublime Text 3

--- common/link-rc-files.sh
+++ common/link-rc-files.sh
@@ -9,7 +9,6 @@ if [ -z $DOTFILES ]; then
 fi
 
 BKUP=".backup.$( date +%s )"
-ST3="$HOME/Library/Application Support/Sublime Text 3"
 
 cd $HOME
 
@@ -29,25 +28,11 @@ ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
 ln -s $DOTFILES/zsh/zshrc.osx.zsh $HOME/.zshrc.osx.zsh
 ln -s $DOTFILES/zsh/zshrc.linux.zsh $HOME/.zshrc.linux.zsh
 
-echo Installing tmux files
-mv -f $HOME/.tmux.conf $HOME/$BKUP.tmux.conf
-mv -f $HOME/.tmux-osx.conf $HOME/$BKUP.tmux-osx.conf
-mv -f $HOME/.tmux-linux.conf $HOME/$BKUP.tmux-linux.conf
-ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
-ln -s $DOTFILES/tmux/tmux-osx.conf $HOME/.tmux-osx.conf
-ln -s $DOTFILES/tmux/tmux-linux.conf $HOME/.tmux-linux.conf
-
 echo Installing git files
 mv -f $HOME/.gitconfig $HOME/$BKUP.gitconfig
 mv -f $HOME/.gitignore_global $HOME/$BKUP.gitignore_global
 ln -s $DOTFILES/git/gitconfig $HOME/.gitconfig
 ln -s $DOTFILES/git/gitignore_global $HOME/.gitignore_global
-
-echo Installing Sublime Text 3 files
-if [[ -d "$ST3" ]]; then
-	mv "$ST3" "$ST3$BKUP.dotfiles"
-fi
-ln -s "$DOTFILES/st3" "$ST3"
 
 echo Installing Atom editor files
 mv -f $HOME/.atom $HOME/$BKUP.atom
