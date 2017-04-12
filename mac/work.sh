05dee414eb484cd10e61d012b1aa8cd7400b0400
Boris Ceranic <boris@catenamedia.com>
2017-04-12 15:14:47 +0200

    Up-to-date package list, as of April 2017

--- mac/work.sh
+++ mac/work.sh
@@ -14,37 +14,30 @@ warn "Press any key to continue. To abort, pressing CTRL+C."
 
 brew cask install 1password
 brew cask install dropbox
-brew cask install firefox
 brew cask install google-chrome
-brew cask install hipchat
 brew cask install java
 brew cask install mysqlworkbench
 brew cask install slack
 brew cask install sourcetree
 brew cask install transmit
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install virtualbox
-brew cask install vmware-fusion
 brew cask install gpgtools
 brew cask install iterm2
-brew cask install karabiner
 brew cask install nvalt
-brew cask install tuntap
 brew install awscli
-brew install boot2docker
-brew install elixir
-brew install erlang
+brew install docker
+brew install docker-compose
+brew install docker-machine
+brew install docker-machine-driver-xhyve
+brew install docker-machine-nfs
 brew install flow
 brew install gnupg2
 brew install gpg-agent
 brew install highlight
 brew install imagemagick
-brew install jenv
 brew install keybase
 brew install mobile-shell
 brew install node
-brew install openvpn
 brew install php56
 brew install php56-intl
-brew install rebar
+brew install php71
+brew install php71-intl
